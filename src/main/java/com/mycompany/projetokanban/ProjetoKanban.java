79ce3ab953aa96e26d112083f338c643e35154c3
Nicolas <nicr3432@gmail.com>
2026-05-08 18:42:06 -0400

    Iniciando o projeto maven e estrutura de pastas.

---
+++ src/main/java/com/mycompany/projetokanban/ProjetoKanban.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projetokanban;
+
+/**
+ *
+ * @author nicolasryan
+ */
+public class ProjetoKanban {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
