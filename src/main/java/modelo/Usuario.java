1f581d476b8e4f0a3e4dc4b3ac6d7f6192026720
Nicolas <243797719+NicolasRyan3432@users.noreply.github.com>
2026-06-14 18:57:56 -0400

    feat: adiciona atributos e métodos na classe Usuario
    Preenche a classe Usuario com os atributos necessários para começar a integração com o banco de dados.

--- src/main/java/modelo/Usuario.java
+++ src/main/java/modelo/Usuario.java
@@ -2,5 +2,59 @@
 package modelo;
 
 public class Usuario {
-    // --- BORA QUERER FELIPE 🙏---
+    private int id;
+    private String login;
+    private String apelido;
+    private String permissao;
+    private String senha;
+    
+    public Usuario() {};
+    
+    public Usuario(int id, String login, String apelido, String permissao, String senha) {
+        this.id = id;
+        this.login = login;
+        this.apelido = apelido;
+        this.permissao = permissao;
+        this.senha = senha;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getApelido() {
+        return apelido;
+    }
+
+    public void setApelido(String apelido) {
+        this.apelido = apelido;
+    }
+
+    public String getPermissao() {
+        return permissao;
+    }
+
+    public void setPermissao(String permissao) {
+        this.permissao = permissao;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 }
